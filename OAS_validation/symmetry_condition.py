5cfd565bf74825974607c6a9e0a300482f809bef
Joaquin Exalto <jexalto@umich.edu>
2022-02-07 20:49:48 -0500

    files makes correct matrix

--- OAS_validation/symmetry_condition.py
+++ OAS_validation/symmetry_condition.py
@@ -13,14 +13,20 @@ span = 10
 jet_loc = 4.0
 jet_radius = 0.75
 
+span = 10.
+crd = 2.
+
 Vinf = 100
 Vjet = 105
 
 ny = 12
 nx = 2
 
-y = np.linspace(0, 10, ny)
-y = (y[1:] + y[0:-1])/2
+y_ = np.linspace(0, span, ny)
+y = (y_[1:] + y_[0:-1])/2
+
+chord_ = np.linspace(0, crd, nx)
+chord = (chord_[1:] + chord_[0:-1])/2
 
 #%% WHAT SIDE OF THE WING DO WE NEED TO CALCULATE?
 right = span - jet_loc
@@ -47,21 +53,22 @@ else:
     y_jet_bool = np.where(y_jet>0, 1, 0)
 
 # --- Make sure we get the normalised y value (normalised over jet radius) ---
-y_n = abs(y-jet_loc)/jet_radius # if y_n<1 --> inside jet
+y_n_ = abs(y_-jet_loc)/jet_radius # if y_n<1 --> inside jet
 if right:
-    y_n = y_n[n_:]
-    c = y_n[0:-1]
-    d = y_n[1:]
+    y_n_ = y_n_[n_:]
+    c = y_n_[0:-1]
+    d = y_n_[1:]
+    eta = abs(y[n_:]-jet_loc)/jet_radius # z/r
 else:
-    y_n = y_n[:N]
-    c = np.flip(y_n[1:])
-    d = np.flip(y_n[0:-1])
+    y_n_ = y_n_[:N]
+    c = np.flip(y_n_[1:])
+    d = np.flip(y_n_[0:-1])
+    eta = abs(y[:N]-jet_loc)/jet_radius # z/r
 
 # %% DEFINE INPUTS FOR RETHORST CORRECTION PACKAGE
 mu = Vinf/Vjet
-ksi = np.ones( (len(y_n)) ) * 1.1
-eta = y_n # z/r
-radius = 1.0
+ksi = chord
+radius = jet_radius
 
 # --- Make entire correction matrix ---
 # First calculate corrections
@@ -70,44 +77,59 @@ goo_out = np.array(0.0)
 goj_out = np.array(0.0)
 gjo_out = np.array(0.0)
 gjj_out = np.array(0.0)
+
 for element in range(N):
     if y_jet_bool[element]==1:
         # J-
         for element2 in range(N):
             if y_jet_bool[element2]==1:
                 # JJ
+                c_ = c[element2]
+                d_ = d[element2]
+                eta_ = eta[element2]
+                ksi = chord[int( (element-0.1)/(ny-1) )]
                 # gjj_odd(mu, ksi, eta, c, d, radius, gjj_out)
-                correction[element, element2] = 0.5 # gjj_out
+                correction[element, element2] = element+element2 # 0.5 # gjj_out
             else:
                 # JO
+                c_ = c[element2]
+                d_ = d[element2]
+                eta_ = eta[element2]
+                ksi = chord[int( (element-0.1)/(ny-1) )]
                 # gjo_odd(mu, ksi, eta, c, d, radius, gjo_out)
-                correction[element, element2] = 0.51 # gjo_out
+                correction[element, element2] = element+element2  # 0.51 # gjo_out
     else:
         # O-
         for element2 in range(N):
             if y_jet_bool[element2]==1:
                 # OJ
+                c_ = c[element2]
+                d_ = d[element2]
+                eta_ = eta[element2]
+                ksi = chord[int( (element-0.1)/(ny-1) )]
                 # goj_odd(mu, ksi, eta, c, d, radius, goj_out)
-                correction[element, element2] = 1.5 # goj_out
+                correction[element, element2] = element+element2 #  1.5 # goj_out
             else:
                 # OO
+                c_ = c[element2]
+                d_ = d[element2]
+                eta_ = eta[element2]
+                ksi = chord[int( (element-0.1)/(ny-1) )]
                 # goo_odd(mu, ksi, eta, c, d, radius, goo_out)
-                correction[element, element2] = 1.51 # goo_out
+                correction[element, element2] = element+element2  # goo_out
+
+correction_ = np.zeros((ny-1, ny-1))
+if right:
+    correction_[0:n_, 0:n_+1] = np.flip(correction[1:n_+1, 0:n_+1])/100
+    correction_[n_:, n_:] = correction[:, :]/100
 
 G_ = np.zeros(( (ny-1)*(nx-1), (ny-1)*(nx-1) ))
-G_1 = np.zeros(( (ny-1)*(nx-1), (ny-1)*(nx-1) ))
+
 right = True
 if right:
     for i in range(nx):
         G_[(ny)*i - i : ny*(i+1)-(i+1), n_ + (ny-1)*i : ny*(i+1)-(i+1)] = 1
-if right:
+else:
     for i in range(nx):
-        G_1[(ny)*i - i : ny*(i+1)-(i+1), (ny-1)*i : n_ + (ny-1)*i] = 1
-
-print(G_ + G_1)
-
-
-# goo_odd(mu, ksi, eta, c, d, radius, goo_out)
-# goj_odd(mu, ksi, eta, c, d, radius, goj_out)
-# gjo_odd(mu, ksi, eta, c, d, radius, gjo_out)
-# gjj_odd(mu, ksi, eta, c, d, radius, gjj_out)
+        G_[(ny)*i - i : ny*(i+1)-(i+1), (ny-1)*i : n_ + (ny-1)*i] = 1
+a = None
